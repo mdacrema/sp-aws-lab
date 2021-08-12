359741278be67653e603a1128c486e817eaa9241
Matteo Dacrema <32163779+mdacrema@users.noreply.github.com>
2021-08-12 11:30:16 +0200

    Add files via upload

--- with_aws_modules/vpc.tf
+++ with_aws_modules/vpc.tf
@@ -6,7 +6,6 @@ module "vpc" {
 
   azs            = ["${local.region}a", "${local.region}b", "${local.region}c"]
   public_subnets = ["172.16.10.0/24", "172.16.11.0/24", "172.16.12.0/24"]
-  #   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   create_igw           = true
   enable_nat_gateway   = false
   enable_vpn_gateway   = false
